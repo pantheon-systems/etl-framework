d7cf8f792349a83f36aec95eeaa38dec9394d588
Michael Liu <michael.liu@getpantheon.com>
2015-12-08 00:09:52 +0000

    Add Extractor fields attribute to config

--- etl_framework/ExtractorConfig.py
+++ etl_framework/ExtractorConfig.py
@@ -3,12 +3,10 @@
 
 from BaseConfig import BaseConfig
 from config_mixins.DestinationMixin import DestinationMixin
-from config_mixins.FieldMappingsMixin import FieldMappingsMixin
 from config_mixins.ExtractorMixin import ExtractorMixin
 from config_mixins.CredentialsMixin import CredentialsMixin
 
 class ExtractorConfig(BaseConfig,
-                    FieldMappingsMixin,
                     ExtratorMixin,
                     CredentialsMixin):
     """parses configuration files"""
--- etl_framework/config_mixins/ExtractorMixin.py
+++ etl_framework/config_mixins/ExtractorMixin.py
@@ -5,10 +5,17 @@ from etl_framework.method_wrappers.check_config_attr import check_config_attr_de
 class SourceMixin(object):
     """parses configuration files"""
 
+    EXTRACTOR_FIELDS_ATTR = 'extractor_fields'
     EXTRACTOR_SYNC_FIELD_ATTR = 'extractor_sync_field'
     EXTRACTOR_TABLE_ATTR = 'extractor_table'
     EXTRACTOR_DELETE_FIELD_ATTR = 'extractor_delete_field'
     EXTRACTOR_UNIQUE_FIELDS_ATTR = 'extractor_unique_fields'
+
+    @check_config_attr_default_none
+    def get_extractor_fields(self):
+        """stuff"""
+
+        return self.config[self.EXTRACTOR_FIELDS_ATTR]
 
     @check_config_attr_default_none
     def get_extractor_sync_field(self):

3216707b8b35a551036717baf7d74f5840dec9ec
Michael Liu <michael.liu@pantheon.io>
2016-07-23 07:36:39 +0000

    Make BufferedMixin database agnostic

--- etl_framework/BufferedLoader.py
+++ etl_framework/BufferedLoader.py
@@ -10,6 +10,16 @@ from loader_mixins.BufferMixin import BufferMixin
 class BufferedLoader(Loader, BufferMixin):
     """loads data into database"""
 
+    def load_buffered(self):
+
+        # NOTE this is specific to SQL loaders,
+        # so this class could be renamed SqlBufferedLoader
+        self.run_statement(
+            self.config.get_sql_statement(),
+            multiple_values=self._buffered_values,
+            commit=True
+        )
+
     def load(self, values):
         """stuff"""
 

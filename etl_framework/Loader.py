501fc9d6c62db6b7c403ed9c834cab809811c605
Michael Liu <michael@michael-onebox.onebox.panth.io>
2016-08-20 00:35:28 +0000

    Restructure imports

--- etl_framework/Loader.py
+++ etl_framework/Loader.py
@@ -4,7 +4,7 @@
 #pylint: disable=too-many-arguments
 #pylint: disable=abstract-class-instantiated
 
-from etl_framework.datastores import MySqlDatabase
+from etl_framework.datastores.mysql_database import MySqlDatabase
 from BaseLoader import BaseLoader
 
 class Loader(

3330d949bf4e611bf31d7d26325107a245a8adad
Harrison Dahme <harrison.dahme@gmail.com>
2016-08-23 14:17:24 -0700

    Update MySqlDatabase.py

--- etl_framework/MySqlDatabase.py
+++ etl_framework/MySqlDatabase.py
@@ -1,1 +1,2 @@
-raise Exception("Class has moved. Import `SqlDatabaseInterface` from `etl_framework.datastore_interfaces.sql_database_interface instead`!")
+raise Exception("Class has moved. Import `SqlDatabaseInterface` from \
+    `etl_framework.datastore_interfaces.sql_database_interface instead`!")

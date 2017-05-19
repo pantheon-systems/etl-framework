4f20484f40bf1d316aa3318bc27c19db9b4422f1
Michael Liu <michael.liu@getpantheon.com>
2017-05-19 22:14:56 +0000

    Squash

--- etl_framework/config_mixins/tests/test_composite_sql_statements.py
+++ etl_framework/config_mixins/tests/test_composite_sql_statements.py
@@ -124,7 +124,7 @@ class CompositeSqlStatementsConfigTestCases(unittest.TestCase):
         )
 
         self.assertEqual(fields, [])
- 
+
         # This is a pretty lame test
         self.assertTrue("UPDATE" in statement.get_sql_clause())
 
